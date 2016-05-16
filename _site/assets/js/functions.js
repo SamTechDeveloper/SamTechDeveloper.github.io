f85e62f108575b9c69c3f5df21e0fd25ae9b88c8
Sameer Ghanekar <samtechdeveloper@gmail.com>
2016-05-16 22:25:06 +0530

    Completed

--- _site/assets/js/functions.js
+++ _site/assets/js/functions.js
@@ -6,6 +6,10 @@ $( document ).ready(function() {
   workLoad();
   clientStuff();
 
+  $("header h1").fitText(1, { minFontSize: '20px', maxFontSize: '72px' });
+  $("a.biglink").fitText(1.5, { minFontSize: '10px', maxFontSize: '72px' });
+
+
 });
 
 function smoothScroll(duration){
@@ -93,3 +97,48 @@ function clientStuff() {
         }
   });
 }
+
+//------ FitText -----------
+/*global jQuery */
+/*!
+* FitText.js 1.2
+*
+* Copyright 2011, Dave Rupert http://daverupert.com
+* Released under the WTFPL license
+* http://sam.zoy.org/wtfpl/
+*
+* Date: Thu May 05 14:23:00 2011 -0600
+*/
+
+(function( $ ){
+
+  $.fn.fitText = function( kompressor, options ) {
+
+    // Setup options
+    var compressor = kompressor || 1,
+        settings = $.extend({
+          'minFontSize' : Number.NEGATIVE_INFINITY,
+          'maxFontSize' : Number.POSITIVE_INFINITY
+        }, options);
+
+    return this.each(function(){
+
+      // Store the object
+      var $this = $(this);
+
+      // Resizer() resizes items based on the object width divided by the compressor * 10
+      var resizer = function () {
+        $this.css('font-size', Math.max(Math.min($this.width() / (compressor*10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)));
+      };
+
+      // Call once to set.
+      resizer();
+
+      // Call on resize. Opera debounces their resize by default.
+      $(window).on('resize.fittext orientationchange.fittext', resizer);
+
+    });
+
+  };
+
+})( jQuery );
